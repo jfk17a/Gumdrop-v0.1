9000ffcedaa621435eb6f4812c49df069bca24d8
jfk17a <44479985+jfk17a@users.noreply.github.com>
2019-12-12 18:41:50 -0600

    Trying to do button stuff

--- app/src/main/java/com/example/gumdropv01/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/gumdropv01/ui/home/HomeViewModel.kt
@@ -1,10 +1,17 @@
 package com.example.gumdropv01.ui.home
 
+import android.widget.Button
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel : ViewModel()  {
+
+    private val _button = MutableLiveData<Button>()
+
+    fun select(button:Button){
+        _button.value = button
+    }
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
